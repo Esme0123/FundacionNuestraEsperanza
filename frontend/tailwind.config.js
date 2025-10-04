372450af4625145164a499546a523f006b664060
Esmeralda <esmeralda.medina.p@ucb.edu.bo>
2025-10-04 13:56:22 -0500

    Configuración de paleta de colores

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'azul-marino':'#12296C',
+        'rosa-principal': '#FF5087',
+        'celeste-fondo': '#59ADD3',
+        'verde-lima': '#88B13F',
+        'turquesa-secundario':'#00C1CA',
+        'amarillo-detalle':'#FFAE00',
+        'celeste-claro':'#ADE5FF',
+        'rosa-claro':'#FFD5E2',
+        'verde-lima':'#F1FFD9',
+        'beige-claro':'#F6F4E9',
+        'amarillo-claro':'#FFE8B7',
+        'white': '#FFFFFF',
+        'black': '#000000',
+      },
+      fontFamily: {
+        
+      } 
+    },
   },
   plugins: [],
 }
